d0b3babf8520359d5b4e487fbf4b0d94d6dae271
Puten <afaputen@list.ru>
2019-05-07 23:25:01 +0300

    log activity lifecycle

--- app/src/main/java/com/example/photogallerybasic/MainActivity.java
+++ app/src/main/java/com/example/photogallerybasic/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.photogallerybasic;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
     private Button backButton;
     private TextView resultText;
     private ImageView imageView;
+    private int randomId;
+
+    public MainActivity() {
+        super();
+        randomId = ThreadLocalRandom.current().nextInt(1, 99);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +38,7 @@ public class MainActivity extends AppCompatActivity {
         resultText = findViewById(R.id.resultTextView);
         imageView = findViewById(R.id.imageView);
 
-        int randomNum = ThreadLocalRandom.current().nextInt(1, 99);
-        resultText.setText("http://myfile.org/" + Integer.toString(randomNum));
+        resultText.setText("http://myfile.org/" + Integer.toString(randomId));
 
         Glide.with(this).load("https://loremflickr.com/320/240")
                 .diskCacheStrategy(DiskCacheStrategy.NONE)
@@ -43,5 +51,129 @@ public class MainActivity extends AppCompatActivity {
         });
 
         backButton.setOnClickListener((view) -> finish());
+
+        String methodName = "onCreate";
+
+        Log.d("Lifecycle " + Integer.toString(randomId), methodName);
+    }
+
+
+    @Override
+    protected void onStart() {
+        String methodName = "onStart";
+
+        Log.d("Lifecycle " + Integer.toString(randomId), methodName);
+        //  if (textView != null) textView.append("\n" + methodName);
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        String methodName = "onResume";
+        super.onResume();
+
+        Log.d("Lifecycle " + Integer.toString(randomId), methodName);
+        //   if (textView != null) textView.append("\n" + methodName);
+    }
+
+    @Override
+    protected void onPause() {
+        String methodName = "onPause";
+        super.onPause();
+
+        Log.d("Lifecycle " + Integer.toString(randomId), methodName);
+        //   if (textView != null) textView.append("\n" + methodName);
+    }
+
+    @Override
+    protected void onStop() {
+        String methodName = "onStop";
+        super.onStop();
+
+        Log.d("Lifecycle " + Integer.toString(randomId), methodName);
+        //  if (textView != null) textView.append("\n" + methodName);
+    }
+
+    @Override
+    protected void onDestroy() {
+        String methodName = "onDestroy";
+        super.onDestroy();
+
+        Log.d("Lifecycle " + Integer.toString(randomId), methodName);
+        //  if (textView != null) textView.append("\n" + methodName);
+    }
+
+    @Override
+    protected void onRestart() {
+        String methodName = "onRestart";
+        super.onRestart();
+
+        Log.d("Lifecycle " + Integer.toString(randomId), methodName);
+        //    if (textView != null) textView.append("\n" + methodName);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        String methodName = "onPostCreate";
+        super.onPostCreate(savedInstanceState);
+
+        Log.d("Lifecycle " + Integer.toString(randomId), methodName);
+        //  if (textView != null) textView.append("\n" + methodName);
+    }
+
+    @Override
+    protected void onPostResume() {
+        String methodName = "onPostResume";
+        super.onPostResume();
+
+        Log.d("Lifecycle " + Integer.toString(randomId), methodName);
+        //   if (textView != null) textView.append("\n" + methodName);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        String methodName = "onKeyDown";
+        Log.d("Lifecycle " + Integer.toString(randomId), methodName + " " + keyCode);
+        //   if (textView != null) textView.append("\n" + methodName + " " + keyCode);
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyLongPress(int keyCode, KeyEvent event) {
+        String methodName = "onKeyLongPress";
+        Log.d("Lifecycle " + Integer.toString(randomId), methodName + " " + keyCode);
+        //    if (textView != null) textView.append("\n" + methodName + " " + keyCode);
+
+        return super.onKeyLongPress(keyCode, event);
+    }
+
+    @Override
+    public void onBackPressed() {
+        String methodName = "onBackPressed";
+        Log.d("Lifecycle " + Integer.toString(randomId), methodName);
+        //    if (textView != null) textView.append("\n" + methodName);
+
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        String methodName = "onSaveInstanceState";
+        Log.d("Lifecycle " + Integer.toString(randomId), methodName);
+        //   if (textView != null) textView.append("\n" + methodName);
+
+
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        String methodName = "onRestoreInstanceState";
+        Log.d("Lifecycle " + Integer.toString(randomId), methodName);
+        //   if (textView != null) textView.append("\n" + methodName);
+
+
+        super.onRestoreInstanceState(savedInstanceState);
     }
 }
